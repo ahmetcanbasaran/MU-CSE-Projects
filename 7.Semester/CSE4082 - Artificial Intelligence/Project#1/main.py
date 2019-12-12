aa2e9527545561b85af344f164493028942954d0
oguzhan-bolukbas <oguzhan.bolukbas18@gmail.com>
2019-12-12 22:35:11 +0300

    BFS and IDS finished.

--- 7.Semester/CSE4082 - Artificial Intelligence/Project#1/main.py
+++ 7.Semester/CSE4082 - Artificial Intelligence/Project#1/main.py
@@ -5,27 +5,28 @@
 #       Oguzhan BOLUKBAS - 150114022        #
 #                                           #
 #############################################
+import queue
 
 
 class Node:
-  def __init__(self, parent, position, depth=0):
+  def __init__(self, parent, position, depth=0, cost=0):
     self.parent = parent  # To keep parent node
     self.position = position  # To keep position of pegs
     self.depth = depth  # To keep depth of node. It is fore IDS
+    self.cost = cost
 
 
 walls = []  # To store walls between squares
 squares = []  # To store squares of maze
-maze_length = -1
+goal_positions = []  # To keep positions of "G" letter squares
+trap_positions = []  # To keep positions of "T" letter squares
+current_position = None  # To keep current position coordinates of agent
+maze_length = -1  # To keep row or column length of the maze
 
 cost_of_solution = -1
-solution_path = []
-expanded_nodes = []  # To keep how many node expanded
-visited_squares = []
-
-current_position = None
-goal_positions = []
-trap_positions = []
+solution_path = []  # To keep solution path
+expanded_nodes = []  # To keep expanded nodes
+visited_squares = []  # To keep visited squares
 
 
 def read_squares(path):
@@ -51,7 +52,8 @@ def read_walls(path):
   fp.close()
 
 
-def find_inital_position():
+def find_square_features():
+  global current_position
   r, c = -1, -1
   for row in squares:
     r += 1
@@ -59,56 +61,38 @@ def find_inital_position():
     for letter in row:
       c += 1
       if letter == "S":
-        return [r, c]
-
-
-def find_goal_position():
-  r, c = -1, -1
-  for row in squares:
-    r += 1
-    c = -1
-    for letter in row:
-      c += 1
-      if letter == "G":
+        current_position = [r, c]
+      elif letter == "G":
         goal_positions.append([r, c])
-
-
-def find_trap_position():
-  r, c = -1, -1
-  for row in squares:
-    r += 1
-    c = -1
-    for letter in row:
-      c += 1
-      if letter == "T":
+      elif letter == "T":
         trap_positions.append([r, c])
 
 
-def agent_mov(x, y, maze):  # To find all possible movements of an agent in current position
-  pass
+def reset_variables():
+  global current_position, cost_of_solution, solution_path, expanded_nodes, visited_squares
+  current_position = None  # To keep current position coordinates of agent
+  cost_of_solution = -1
+  solution_path = []  # To keep solution path
+  expanded_nodes = []  # To keep expanded nodes
+  visited_squares = []  # To keep visited squares
 
 
 # To find possible squares the agent can move
 def possible_positions(position):
   pos_moves = []
   x, y = position[0], position[1]  # Current coordinates
-
   if position[0] != 0:  # NORTH
     if walls[x][y][3] == "O":
       pos_moves.append([position[0] - 1, position[1]])
-
-  if position[1] != 0:  # WEST
-    if walls[x][y][2] == "O":
-      pos_moves.append([position[0], position[1] - 1])
-
-  if position[0] != maze_length - 1:  # SOUTH
-    if walls[x][y][1] == "O":
-      pos_moves.append([position[0] + 1, position[1]])
-
   if position[1] != maze_length - 1:  # EAST
     if walls[x][y][0] == "O":
       pos_moves.append([position[0], position[1] + 1])
-
+  if position[0] != maze_length - 1:  # SOUTH
+    if walls[x][y][1] == "O":
+      pos_moves.append([position[0] + 1, position[1]])
+  if position[1] != 0:  # WEST
+    if walls[x][y][2] == "O":
+      pos_moves.append([position[0], position[1] - 1])
   return pos_moves
 
 
@@ -139,51 +123,86 @@ def is_trap(position):
 def find_solution_path(node):
   global solution_path, cost_of_solution
   while node:
-    if is_trap(node.position): cost_of_solution += 7
-    else: cost_of_solution += 1
+    if is_trap(node.position):
+      cost_of_solution += 7
+    else:
+      cost_of_solution += 1
     solution_path.append(node.position)
     node = node.parent
   solution_path.reverse()
 
+
+# Depth First Search
 def dfs():
   global current_position, cost_of_solution, visited_squares
-  current_position = find_inital_position()
   frontier = []  # Stack for DFS. Use append() and pop()
-  initial_node = Node(None, current_position, 0)
+  initial_node = Node(None, current_position)
   frontier.append(initial_node)
-
   while True:
     if not frontier:
       print("Frontier is empty. There is no solution")
       return None
-
     parent = frontier.pop()
     expanded_nodes.append(parent.position)
-
     if is_goal(parent.position):
       find_solution_path(parent)
       return
-
     visited_squares.append(parent.position)
     p_positions = possible_positions(parent.position)
-
     for position in p_positions:
       if not is_visited(position):
         child = Node(parent, position, parent.depth + 1)
         frontier.append(child)
 
 
-# TODO: Breadth First Search
+# Breadth First Search
 def bfs():
-  pass
+  global current_position, cost_of_solution, visited_squares
+  frontier = queue.Queue()  # Stack for DFS. Use append() and pop()
+  initial_node = Node(None, current_position)
+  frontier.put(initial_node)
+  while True:
+    if frontier.empty():
+      print("Frontier is empty. There is no solution")
+      return None
+    parent = frontier.get()
+    expanded_nodes.append(parent.position)
+    if is_goal(parent.position):
+      find_solution_path(parent)
+      return
+    visited_squares.append(parent.position)
+    p_positions = possible_positions(parent.position)
+    p_positions.reverse()  # To get moves according to ordered directions
+    for position in p_positions:
+      if not is_visited(position):
+        child = Node(parent, position, parent.depth + 1)
+        frontier.put(child)
 
 
-# TODO: Iterative Deepening
+# Iterative Deepening Search
 def ids():
-  pass
+  global current_position, cost_of_solution, visited_squares
+  initial_node = Node(None, current_position)
+  for depth in range(maze_length * maze_length):
+    reset_variables()
+    frontier = queue.Queue()  # Stack for DFS. Use append() and pop()
+    frontier.put(initial_node)
+    while not frontier.empty():
+      parent = frontier.get()
+      expanded_nodes.append(parent.position)
+      if is_goal(parent.position):
+        find_solution_path(parent)
+        return
+      visited_squares.append(parent.position)
+      p_positions = possible_positions(parent.position)
+      p_positions.reverse()
+      if parent.depth < depth:
+        for position in p_positions:
+          if not is_visited(position):
+            child = Node(parent, position, parent.depth + 1)
+            frontier.put(child)
 
 
-# TODO: Uniform Cost Search
 def ucs():
   pass
 
@@ -203,43 +222,45 @@ def main():
   input_walls_path = "./inputs/maze_1/walls.txt"
   read_squares(input_squares_path)
   read_walls(input_walls_path)
-  find_goal_position()
-  find_trap_position()
+  find_square_features()
 
   # WARNING: Order of node expansion should be East, South, West, North
-  dfs()
+  # dfs()
+  # bfs()
+  # ids()
+  # ucs()
+
+  """
+  print("a. Depth First Search\n" +
+        "b. Breadth First Search\n" +
+        "c. Iterative Deepening Search\n" +
+        "d. Depth First Search with Random Selection\n" +
+        "e. Depth First Search with a Node Selection Heuristic")
+  method = input("Choose a search method: ")
+  if method == 'a':
+    print("\nSearch method is: Depth First Search")
+    dfs()
+  elif method == 'b':
+    print("\nSearch method is: Breadth First Search")
+    bfs()
+  elif method == 'c':
+    print("\nSearch method is: Iterative Deepening Search")
+    ids()
+  elif method == 'd':
+    print("\nSearch method is: Uniform Cost Search")
+    ucs()
+  elif method == 'r':
+    print("\nSearch method is: Greedy Best First Search")
+    gbfs()
+  elif method == 'f':
+    print("\nSearch method is: A* Heuristic Search")
+    ashs()
+  """
+
+  # To print result
   print("The cost of the solution: \n", cost_of_solution, "\n")
   print("The solution path is: \n", solution_path, "\n")
   print("Expanded nodes are: \n", expanded_nodes, "\n")
-
-  """
-  print("a. Depth First Search\nb. Breadth First Search\nc. Iterative Deepening Search\nd. Depth First Search with Random Selection\ne. Depth First Search with a Node Selection Heuristic")
-
-  method = input("Choose a search method: ")
-  if (method == 'a'):
-    print("\nSearch method is: Depth First Search")
-    dfs()
-
-  elif (method == 'b'):
-    print("\nSearch method is: Breadth First Search")
-    node = bfs()
-
-  elif (method == 'c'):
-    print("\nSearch method is: Iterative Deepening Search")
-    node = ids()
-
-  elif (method == 'd'):
-    print("\nSearch method is: Uniform Cost Search")
-    node = ucs()
-
-  elif (method == 'r'):
-    print("\nSearch method is: Greedy Best First Search")
-    node = gbfs()
-
-  elif (method == 'f'):
-    print("\nSearch method is: A* Heuristic Search")
-    node = ashs()
-  """
 
 
 if __name__ == '__main__':
