989088c1d6c7116b24e5b34c838654e81a10edf7
oguzhan-bolukbas <oguzhan.bolukbas18@gmail.com>
2020-01-08 23:14:22 +0300

    Some optimizations done.

--- 7.Semester/CSE4082 - Artificial Intelligence/Project#2/main.py
+++ 7.Semester/CSE4082 - Artificial Intelligence/Project#2/main.py
@@ -16,6 +16,7 @@ weight_array = []  # To keep weight values of vertices
 adjacency_matrix = None  # To fill adjacency matrix with zeros
 checklist = []  # To store edges with node numbers
 
+
 def generate_random_pop():
   population = []
   for i in range(0, number_of_pop):
@@ -29,6 +30,21 @@ def generate_random_pop():
     population.append(offspring)
   return population
 
+
+"""
+def generate_random_pop():
+  population = []
+  for i in range(0, number_of_pop):
+    offspring = []
+    #generate offsprings with uniform random distribution
+    for j in range(0, number_of_nodes):
+      if uniform(0, 1) < 0.5:
+        offspring.append(0)
+      else:
+        offspring.append(1)
+    population.append(offspring)
+  return population
+"""
 
 def repair(population):
   fit_value = 0
@@ -54,7 +70,7 @@ def repair(population):
       else:  # This means every edges are not covered
         changes = []
         print('uzunluk2', len(temp_checklist))
-        #put nodes that at the end of uncovered edges in a list 
+        #put nodes that at the end of uncovered edges in a list
         for k in temp_checklist:
           changes.append(k[0])
           changes.append(k[1])
@@ -63,12 +79,12 @@ def repair(population):
         population[i][changes[rand]] = 1
     print("Offspring {} is finished".format(i))
   average_fit = fit_value / number_of_pop  #To calculate average fit
-  return population, average_fit
+  return average_fit
 
 
-def matching_pool_tournement(population, weight_array):
+def mating_pool_tournement(population):
   pop_size = number_of_pop
-  matching_pool = []
+  mating_pool = []
   for i in range(0, pop_size):
     fitness = 100000000
     winner = None
@@ -81,39 +97,28 @@ def matching_pool_tournement(population, weight_array):
       if new_fitness < fitness:
         fitness = new_fitness
         winner = rand
-    matching_pool.append(population[winner])
-  return matching_pool
+    mating_pool.append(population[winner])
+  return mating_pool
 
 
-def crossover(matching_pool, crossover_prob, number_of_nodes):
-  crossoverred_pop = []
-  i = -1
-  for j in range(0, number_of_pop):
-    i += 2
+def crossover(population):
+  for i in range(1, number_of_pop+1, 2):
     rand = uniform(0, 1)
     #if random number is smaller than crossover probability make 2 child with 1 point crossover
     if rand <= crossover_prob:
       rand2 = randrange(0, number_of_nodes)
-      child1 = matching_pool[i - 1][:rand2] + matching_pool[i][rand2:]
-      child2 = matching_pool[i][:rand2] + matching_pool[i - 1][rand2:]
-      crossoverred_pop.append(child1)
-      crossoverred_pop.append(child2)
-    #else transfer parents to to new population with no change
-    else:
-      crossoverred_pop.append(matching_pool[i - 1])
-      crossoverred_pop.append(matching_pool[i])
-    if i == number_of_pop - 1:
-      break
-  return crossoverred_pop
+      child1 = population[i - 1][:rand2] + population[i][rand2:]
+      child2 = population[i][:rand2] + population[i - 1][rand2:]
+      population[i-1] = child1
+      population[i] = child2
 
 
-def mutate(crossoverred_pop, mutation_prob, number_of_nodes):
+def mutate(population):
   for i in range(0, number_of_pop):
     for j in range(0, number_of_nodes):
       #if random number is smaller than mutation probability change the responsible bit as opposite   
       if uniform(0, 1) < mutation_prob:
-        crossoverred_pop[i][j] = 1 - crossoverred_pop[i][j]
-  return crossoverred_pop
+        population[i][j] = 1 - population[i][j]
 
 
 def main():
@@ -129,8 +134,8 @@ def main():
   """
 
   file_name = "003.txt"
-  number_of_gen = 10
-  number_of_pop = 10
+  number_of_gen = 5
+  number_of_pop = 4
   crossover_prob = 0.5
   mutation_prob = 0.05
 
@@ -160,24 +165,25 @@ def main():
         checklist.append([i, j])
 
   population = generate_random_pop()  # To produce the first population randommly
-  population, average_fit = repair(population)  # To repair it and get average fit value
+  average_fit = repair(population)  # To repair it and get average fit value
 
   i = 0
   average_fit_values = [average_fit]
   while i != number_of_gen:
-    matching_pool = matching_pool_tournement(population, weight_array)    # To form a matching pool with possible parents
-    crosseverred_array = crossover(matching_pool, crossover_prob, number_of_nodes)   # To crossover for parents that give child
-    mutationed_array = mutate(crosseverred_array, mutation_prob, number_of_nodes)     # To mutate some bits in childs
-    population, average_fit = repair(mutationed_array)    # To repair it and get average fit value
+    population = mating_pool_tournement(population)    # To form a mating pool with possible parents
+    crossover(population)   # To crossover for parents that give child
+    mutate(population)     # To mutate some bits in childs
+    average_fit = repair(population)    # To repair it and get average fit value
     average_fit_values.append(average_fit)
     i += 1
     print("Generation", i , "is finished")
 
+  generation = []
   print(average_fit_values)
   for i in range(0,len(average_fit_values)):
       generation.append(i)
 
-  plt.plot(generation,average_fit_values)
+  plt.plot(generation, average_fit_values)
   plt.xlabel('Generation')
   plt.ylabel('Average Fitness')
   plt.title('Generation-Average Fitness Graph of '+str(file_name)+' with popsize:'+str(number_of_pop)+', crossover probability:'+str(crossover_prob) +' and mutation probability:'+str(mutation_prob))
