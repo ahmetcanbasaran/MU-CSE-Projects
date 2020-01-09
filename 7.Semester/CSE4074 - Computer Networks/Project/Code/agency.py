bb4a0e26c061a624ec6c17d6ad1a43306b0da7f0
oguzhan-bolukbas <oguzhan.bolukbas18@gmail.com>
2020-01-09 16:36:06 +0300

    Suggestion system done for hotels.

--- 7.Semester/CSE4074 - Computer Networks/Project/Code/agency.py
+++ 7.Semester/CSE4074 - Computer Networks/Project/Code/agency.py
@@ -23,42 +23,36 @@ hotel_name = None
 airline_name = None
 number_of_travelers = None
 reservated = None
+master = None  # For GUI
+reservation_success = None
+
+def rejected_gui():
+  global is_suggestion_accepted
+  is_suggestion_accepted = False
+  master.destroy()
 
 
-def reservation(arrival_date, hotel_name, number_of_travelers):
-  try:  # To connect to server via TCP connection
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-    print 'Hotel socket generated'
-  except socket.error, msg:
-    print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
-    sys.exit()
+def accepted_gui():
+  global is_suggestion_accepted
+  is_suggestion_accepted = True
+  master.destroy()
 
-  host = 'localhost'
-  if hotel_name == "Rixos":
-    port = 9091
-  else:
-    port = 9092
 
-  try:
-    s.connect((host, port))  # To connect to the server on local computer
-  except socket.error, msg:
-    print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
-    s.close()
-    return
+def gui_suggestion(first_arrival_date, last_arrival_date, hotel_name, number_of_travelers):
+  global master
+  arrival_dates = str(first_arrival_date) + "-" + str(last_arrival_date)
 
-  try:
-    hotels_request = hotel_name + " " + str(number_of_travelers) + " " + str(arrival_date)
-    s.send(hotels_request)  # To send the hotels
-  except socket.error:
-    print 'Send failed'
-    s.close()
-    return
+  tk.Label(master, text="---SUGGESTION---").grid(row=0)
+  tk.Label(master, text="Departure date: ").grid(row=1)
+  tk.Label(master, text="Arrival date: " + arrival_dates).grid(row=2)
+  tk.Label(master, text="Hotel name: " + hotel_name).grid(row=3)
+  tk.Label(master, text="Airline name: ").grid(row=4)
+  tk.Label(master, text="Number of travelers: " + str(number_of_travelers)).grid(row=5)
 
-  response = s.recv(32768)  # receive data from the server
-  print "\nReceived message from the hotels: ", response
+  tk.Button(master, text='Accept', command=accepted_gui).grid(row=6, column=0, sticky=tk.W, pady=4)
+  tk.Button(master, text='Reject', command=rejected_gui).grid(row=6, column=1, sticky=tk.W, pady=4)
 
-  s.close()  # To close the connection to hotels
-  return response
+  master.mainloop()
 
 
 def learn_capacities():
@@ -114,34 +108,6 @@ def learn_capacities():
   return [rixos_capacity, hilton_capacity]
 
 
-def rejected_gui():
-  global is_suggestion_accepted
-  is_suggestion_accepted = False
-
-
-def accepted_gui():
-  global is_suggestion_accepted
-  is_suggestion_accepted = True
-  master.destroy()
-
-
-def gui_suggestion(first_arrival_date, last_arrival_date, hotel_name, number_of_travelers):
-  global master
-  arrival_dates = str(first_arrival_date) + "-" + str(last_arrival_date)
-
-  tk.Label(master, text="---SUGGESTION---").grid(row=0)
-  tk.Label(master, text="Departure date: ").grid(row=1)
-  tk.Label(master, text="Arrival date: " + arrival_dates).grid(row=2)
-  tk.Label(master, text="Hotel name: " + hotel_name).grid(row=3)
-  tk.Label(master, text="Airline name: ").grid(row=4)
-  tk.Label(master, text="Number of travelers: " + str(number_of_travelers)).grid(row=5)
-
-  tk.Button(master, text='Accept', command=accepted_gui).grid(row=6, column=0, sticky=tk.W, pady=4)
-  tk.Button(master, text='Reject', command=rejected_gui).grid(row=6, column=1, sticky=tk.W, pady=4)
-
-  master.mainloop()
-
-
 def find_suggestions(arrival_date, departure_date, number_of_travelers):
   capacities = learn_capacities()
   rixos_capacities = capacities[0].split("-")
@@ -153,6 +119,7 @@ def find_suggestions(arrival_date, departure_date, number_of_travelers):
   how_long_the_trip = last_arrival_date - first_arrival_date + 1
 
   # To suggest another trip days in Rixos Hotel
+  global master
   for dates in range(0, len(rixos_capacities) - how_long_the_trip + 1, 1):
     is_suitable = True
     for days in range(dates, dates + how_long_the_trip, 1):
@@ -160,12 +127,13 @@ def find_suggestions(arrival_date, departure_date, number_of_travelers):
         is_suitable = False
     if is_suitable:
       print "Suitable days in Rixos from ", dates, " to ", dates + how_long_the_trip - 1
+      master = tk.Tk()  # For GUI
       gui_suggestion(dates, dates + how_long_the_trip - 1, "Rixos", number_of_travelers)
       if is_suggestion_accepted:
         new_days = str(dates) + "-" + str(dates + how_long_the_trip - 1)
         hotels_response = reservation(new_days, "Rixos", number_of_travelers)
         c.send(hotels_response)
-        return
+        return True
 
   # To suggest another trip days in Hilton Hotel
   for dates in range(0, len(hilton_capacities) - how_long_the_trip + 1, 1):
@@ -173,16 +141,53 @@ def find_suggestions(arrival_date, departure_date, number_of_travelers):
     for days in range(dates, dates + how_long_the_trip, 1):
       if number_of_travelers > int(hilton_capacities[days]):
         is_suitable = False
+        master.destroy()
     if is_suitable:
       print "Suitable days in Hilton from ", dates, " to ", dates + how_long_the_trip - 1
+      master = tk.Tk()  # For GUI
       gui_suggestion(dates, dates + how_long_the_trip - 1, "Hilton", number_of_travelers)
       if is_suggestion_accepted:
         new_days = str(dates) + "-" + str(dates + how_long_the_trip - 1)
         hotels_response = reservation(new_days, "Hilton", number_of_travelers)
         c.send(hotels_response)
-        return
+        return True
+  return False
 
-  return
+
+def reservation(arrival_date, hotel_name, number_of_travelers):
+  try:  # To connect to server via TCP connection
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+    print 'Hotel socket generated'
+  except socket.error, msg:
+    print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
+    sys.exit()
+
+  host = 'localhost'
+  if hotel_name == "Rixos":
+    port = 9091
+  else:
+    port = 9092
+
+  try:
+    s.connect((host, port))  # To connect to the server on local computer
+  except socket.error, msg:
+    print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
+    s.close()
+    return
+
+  try:
+    hotels_request = hotel_name + " " + str(number_of_travelers) + " " + str(arrival_date)
+    s.send(hotels_request)  # To send the hotels
+  except socket.error:
+    print 'Send failed'
+    s.close()
+    return
+
+  response = s.recv(32768)  # receive data from the server
+  print "\nReceived message from the hotels: ", response
+
+  s.close()  # To close the connection to hotels
+  return response
 
 
 def reservation_request():
@@ -249,7 +254,9 @@ def main():
       # thread.start_new_thread(reservation_request, ()); time.sleep(10)
       reservation_request()
       if not reservated:
-        find_suggestions(arrival_date, departure_date, number_of_travelers)
+        reservation_success = find_suggestions(arrival_date, departure_date, number_of_travelers)
+        if not reservation_success:
+          c.send("Suggestions rejected!")
     except KeyboardInterrupt:
       s.close()
       print "\nServer is shutting down"
--- 7.Semester/CSE4074 - Computer Networks/Project/Code/rixos_hotel.py
+++ 7.Semester/CSE4074 - Computer Networks/Project/Code/rixos_hotel.py
@@ -34,7 +34,6 @@ def make_reservation(first_arrival_date, last_arrival_date, num_of_travelers):
 
 def check_reservation(c):
   message = c.recv(32768)
-
   if message == "What is your capacity?":
     capacity = str(hotel_capacity[0]) + "-" + str(hotel_capacity[1]) + "-" + str(hotel_capacity[2])
     c.send(capacity)  # Return hotel capacity to make available suggestion
@@ -49,7 +48,7 @@ def check_reservation(c):
     for dates in range(first_arrival_date, last_arrival_date+1, 1):  # To check capacities
       if num_of_travelers > hotel_capacity[dates]:
         is_capacity_enough = False
-    if is_capacity_enough:  # Reservation can done
+    if is_capacity_enough:  # Reservation can be done
       response = make_reservation(first_arrival_date, last_arrival_date, num_of_travelers)
       c.send(response)
     else:  # Capacity is not enough
@@ -89,7 +88,6 @@ def main():
       c, addr = s.accept()  # To establish connection with client. Server waits here
       print "Connected with", addr[0], ":", str(addr[1])
       check_reservation(c)  # To check reservation whether is available
-
     except KeyboardInterrupt:
       s.close()
       print "\nServer is shutting down"
